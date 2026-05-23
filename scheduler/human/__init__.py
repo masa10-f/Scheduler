2b738496881cbfe8f1b6352ec5f9a727036a9eaf
Masato Fukushima <masa1063fuk@gmail.com>
2026-05-23 13:17:44 -0400

    Add HumanCompiler scheduler boundary models

---
+++ scheduler/human/__init__.py
@@ -0,0 +1,21 @@
+"""Experimental HumanCompiler scheduling domain API."""
+
+from .model import (
+    HumanDailyPlan,
+    HumanFixedAssignment,
+    HumanScheduleBlock,
+    HumanTask,
+    HumanTaskSource,
+    HumanTimeSlot,
+    HumanWorkKind,
+)
+
+__all__ = [
+    "HumanDailyPlan",
+    "HumanFixedAssignment",
+    "HumanScheduleBlock",
+    "HumanTask",
+    "HumanTaskSource",
+    "HumanTimeSlot",
+    "HumanWorkKind",
+]
